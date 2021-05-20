23599f1de375a0f5107383060c70477d4eb2c5a5
JuliusA <julius.soltani@gmail.com>
2021-05-20 22:52:24 +0200

    Added a weather API and changed the information in ReadMe

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
 import { SocialIcon } from "react-social-icons";
 
+
+const api = {
+    key: "0c145d2701598a41a9f7e2604e6f793c",
+    base: "http://api.openweathermap.org/data/2.5/"
+}
+
+
 export default function NavBar() {
+
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+    const search = evt => {
+        if (evt.key === "Enter") {
+            fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+                .then(res => res.json())
+                .then(result => { setQuery(''); setWeather(result) });
+        }
+    }
+
+    const dateBuilder = (d) => {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day} ${date} ${month} ${year}`
+    }
+
     return (
         <header className="bg-blue-600">
             <div className="container mx-auto flex justify-between">
@@ -29,11 +59,35 @@ export default function NavBar() {
                         About Me!
                     </NavLink>
                 </nav>
-                <div className="inline-flex py-3 px-3 my-6">
-                    <SocialIcon url="https://github.com/Julius-88" className="mr-4" target="_blank" fgColor="#fff" rel="noopener noreferrer" style={{ height: 35, width: 35 }} />
-                    <SocialIcon url="https://www.linkedin.com/in/julius-alamarvdashti-38649270/" className="mr-4" rel="noopener noreferrer" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+                <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+                    <SocialIcon url="https://github.com/Julius-88" className="my-8 mx-5  " target="_blank" fgColor="#fff" rel="noopener noreferrer" mt-8 style={{ height: 35, width: 35 }} />
+                    <SocialIcon url="https://www.linkedin.com/in/julius-alamarvdashti-38649270/" className="" rel="noopener noreferrer" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
                 </div>
+                <div className="search-box">
+                    <input type="text"
+                        className="search-bar"
+                        placeholder="Find out your local weather"
+                        onChange={e => setQuery(e.target.value)}
+                        value={query}
+                        onKeyPress={search}
+                    />
+                </div>
+                {(typeof weather.main != "undefined") ? (
+                    <div>
+                        <div className="location-box">
+                            <div className="location">{weather.name}, {weather.sys.country} </div>
+                            <div className="date">{dateBuilder(new Date())} </div>
+                        </div>
+                        <div className="weather-box">
+                            <div className="temp">
+                                {Math.round(weather.main.temp)}&deg;C
+                            </div>
+                            <div className="weather">{weather.weather[0].main} </div>
+                        </div>
+                    </div>
+                ) : ('')}
             </div>
-        </header>
+
+        </header >
     )
 }
